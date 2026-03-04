ffed67668a0220a6dd974bac4cbc2a992b768079
Yushu Qin <66906356+last-Medjay@users.noreply.github.com>
2026-03-04 16:11:03 +0000

    Refactor PopCount and ArgMax implementations

--- src/CLAP.sv
+++ src/CLAP.sv
@@ -8,14 +8,22 @@ module CLAP #(CLAUSES=64, CLASSES=5) (
 );
 
 logic unsigned [$clog2(CLAUSES)-1:0] sum [0:CLASSES-1];
+logic unsigned [CLASSES*$clog2(CLAUSES)-1:0] sum_flat;
 
-PopCount pc0 (.clk, .rstn, .CL(CL[CLAUSES-1:0]), .sum(sum[0]));
-PopCount pc1 (.clk, .rstn, .CL(CL[2*CLAUSES-1:CLAUSES]), .sum(sum[1]));
-PopCount pc2 (.clk, .rstn, .CL(CL[3*CLAUSES-1:2*CLAUSES]), .sum(sum[2]));
-PopCount pc3 (.clk, .rstn, .CL(CL[4*CLAUSES-1:3*CLAUSES]), .sum(sum[3]));
-PopCount pc4 (.clk, .rstn, .CL(CL[5*CLAUSES-1:4*CLAUSES]), .sum(sum[4]));
+genvar i;
+generate
+    for (i = 0; i < CLASSES; i++) begin : sum_flatten
+        assign sum_flat[i*$clog2(CLAUSES) +: $clog2(CLAUSES)] = sum[i];
+    end
+endgenerate
 
-ArgMax am0 (.clk, .rstn, .sum(sum), .argmax(argmax));
+PopCount pc0 (.clk, .rstn, .CL(CL[CLAUSES-1:0]),         .sum(sum[0]));
+PopCount pc1 (.clk, .rstn, .CL(CL[2*CLAUSES-1:CLAUSES]),  .sum(sum[1]));
+PopCount pc2 (.clk, .rstn, .CL(CL[3*CLAUSES-1:2*CLAUSES]),.sum(sum[2]));
+PopCount pc3 (.clk, .rstn, .CL(CL[4*CLAUSES-1:3*CLAUSES]),.sum(sum[3]));
+PopCount pc4 (.clk, .rstn, .CL(CL[5*CLAUSES-1:4*CLAUSES]),.sum(sum[4]));
+
+ArgMax am0 (.clk, .rstn, .sum(sum_flat), .argmax(argmax));
 
 endmodule
 
@@ -46,15 +54,16 @@ module ArgMax #(CLAUSES=64, CLASSES=5) (
 );
 
 logic unsigned [$clog2(CLAUSES)-1:0] sum_intern [0:CLASSES-1];
-logic unsigned [$clog2(CLAUSES)-1:0] MaxSum;
-logic unsigned [CLASSES-1:0] MaxClass;
 
 genvar i;
 generate
-    for (i = 0; i < CLASSES; i++) begin : sum_assign
+    for (i = 0; i < CLASSES; i++) begin : sum_unpack
         assign sum_intern[i] = sum[i*$clog2(CLAUSES) +: $clog2(CLAUSES)];
     end
 endgenerate
+
+logic unsigned [$clog2(CLAUSES)-1:0] MaxSum;
+logic unsigned [CLASSES-1:0] MaxClass;
 
 always_comb begin
     MaxSum = sum_intern[0];
@@ -65,7 +74,7 @@ always_comb begin
     end
 
     MaxClass = 0;
-    for (int i = 0; i < 5; i++) begin
+    for (int i = 0; i < CLASSES; i++) begin
         if (sum_intern[i] == MaxSum) begin
             MaxClass[i] = 1;
         end
@@ -75,4 +84,3 @@ always_comb begin
 end
 
 endmodule
-
